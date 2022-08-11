8683782997e56ee8df81a56e77ca0b5bfbc08904
lucleber13 <lucleber13@hotmail.com>
2022-08-11 01:04:52 +0100

    Controller class created and updates

--- src/main/java/com/cbcode/projectfinal/payload/request/SignUpRequest.java
+++ src/main/java/com/cbcode/projectfinal/payload/request/SignUpRequest.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
-public class SignupRequest {
+public class SignUpRequest {
 
     @NotBlank
     @Size(min = 3, max = 20)
--- src/main/java/com/cbcode/projectfinal/repository/RoleRepository.java
+++ src/main/java/com/cbcode/projectfinal/repository/RoleRepository.java
@@ -1,5 +1,6 @@
 package com.cbcode.projectfinal.repository;
 
+import com.cbcode.projectfinal.models.ERole;
 import com.cbcode.projectfinal.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Optional<Role> findByName(String name);
+    Optional<Role> findByName(ERole name);
 }
